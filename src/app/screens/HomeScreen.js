87ed1635d8b9934effaaddc3c3a038a9fb8371ee
unknown <joeyskaff02@gmail.com>
2021-09-09 13:28:57 +0300

    new

--- src/app/screens/HomeScreen.js
+++ src/app/screens/HomeScreen.js
@@ -1,18 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 function HomeScreen(props) {
     return (
-        <View style={styles.container}>
-            <Text>HomeScreen</Text>
+        <View style={styles.pageContainer}>
+           <View style={styles.card}>
+
+              <ImageBackground 
+                    source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png' }} style={{}}
+                    style={styles.image}>
+                        <View style={styles.cardInner}>
+
+                            <Text style={styles.name} >Elon Musk</Text>
+                            <Text style={styles.bio} >A dude with a rocket is looking for a gal with fuel</Text>
+
+                        </View>
+               </ImageBackground>
+
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-       flex: 1,
-   } 
-});
+    pageContainer: {
+      justifyContent: 'center',
+       alignItems: 'center',
+        flex: 1,
+      },
+      image: {
+        width: '100%', 
+        height: '100%',
+        borderRadius: 10,
+        overflow: 'hidden',
+
+        justifyContent: 'flex-end',
+      },
+      cardInner: {
+          padding: 10,
+      },
+      card: {
+        width: '95%',
+        height: '70%',
+        borderRadius: 10,
+  
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 5,
+        },
+        shadowOpacity: 0.36,
+        shadowRadius: 6.68,
+  
+        elevation: 11,
+      },
+      name: {
+          fontSize: 30,
+          color: 'white',
+          fontWeight: 'bold',
+      },
+      bio: {
+        fontSize: 18,
+        color: 'white',
+        lineHeight: 25,
+      },
+  })
 
 export default HomeScreen;
