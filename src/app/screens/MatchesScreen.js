9aac9181cab2901fb5ce36f4e440f3852b8497c4
michaelkozah <86603711+michaelkozah@users.noreply.github.com>
2021-09-10 15:29:11 +0300

    minor bugs

--- src/app/screens/MatchesScreen.js
+++ src/app/screens/MatchesScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { Component } from 'react';
+import { Button, SwitchComponent } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements';
+import { ListItem, Avatar, Badge } from 'react-native-elements';
 import TouchableScale from 'react-native-touchable-scale';
 import { ViewPropTypes } from 'react-native-web';
 
@@ -13,6 +13,7 @@ function MatchesScreen(props) {
             friction={5}
             tension={40}
             activeScale={0.95}
+            button onPress={() => console.log('pressed1')}
             >
                 <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
                 <ListItem.Content>
@@ -31,6 +32,35 @@ function MatchesScreen(props) {
             friction={5}
             tension={40}
             activeScale={0.95}
+            button onPress={() => console.log('pressed2')}
+            >
+                <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
+                <ListItem.Content>
+                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
+                    Chris Jackson
+                </ListItem.Title>
+                <ListItem.Subtitle style={{ color: 'black' }}>
+                    Wants to play Football with you!
+                </ListItem.Subtitle>
+                </ListItem.Content>
+                    <Badge 
+                    style={styles.badge}
+                    status="error" 
+                    
+                    />
+                
+                
+                    
+                <ListItem.Chevron color="black" />
+                
+                
+            </ListItem>
+            <ListItem
+            Component={TouchableScale}
+            friction={5}
+            tension={40}
+            activeScale={0.95}
+            button onPress={() => console.log("pressed3")}
             >
                 <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
                 <ListItem.Content>
@@ -49,8 +79,17 @@ function MatchesScreen(props) {
             friction={5}
             tension={40}
             activeScale={0.95}
+            button onPress={() => console.log('pressed4')}
+            onLongPress={() => console.log('long pressed')}
             >
+                <View>
                 <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
+                <Badge 
+                //value="online"
+                status="success"
+                containerStyle={{ position: 'absolute', top: 0, right: -1 }}
+                />
+                </View>
                 <ListItem.Content>
                 <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
                     Chris Jackson
@@ -61,23 +100,9 @@ function MatchesScreen(props) {
                 </ListItem.Content>
                 <ListItem.Chevron color="black" />
                 
-            </ListItem>
-            <ListItem
-            Component={TouchableScale}
-            friction={5}
-            tension={40}
-            activeScale={0.95}
-            >
-                <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
-                <ListItem.Content>
-                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
-                    Chris Jackson
-                </ListItem.Title>
-                <ListItem.Subtitle style={{ color: 'black' }}>
-                    Wants to play Football with you!
-                </ListItem.Subtitle>
-                </ListItem.Content>
-                <ListItem.Chevron color="black" />
+                
+
+                
                 
             </ListItem>
         </View>
@@ -87,7 +112,8 @@ function MatchesScreen(props) {
 const styles = StyleSheet.create({
     container: {
        flex: 1,
-   } 
+   },
+  
 });
 
 export default MatchesScreen;
