3d916ad9a9739567825f3b35e3c16212f76f8c67
William-Baaklini <76127831+William-Baaklini@users.noreply.github.com>
2021-09-13 01:15:48 -0700

    Squashed commit of the following:
    commit 9aac9181cab2901fb5ce36f4e440f3852b8497c4
    Author: michaelkozah <86603711+michaelkozah@users.noreply.github.com>
    Date:   Fri Sep 10 15:29:11 2021 +0300
    
        minor bugs
    
    commit 4aa9bc62d912a0d9cd1b35cb78ffa27bcaacee1c
    Author: michaelkozah <86603711+michaelkozah@users.noreply.github.com>
    Date:   Thu Sep 9 19:08:23 2021 +0300
    
        Displaying matches
    
        Displaying matches and future messages on MatchesScreen

--- src/app/screens/MatchesScreen.js
+++ src/app/screens/MatchesScreen.js
@@ -1,10 +1,110 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, SwitchComponent } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { ListItem, Avatar, Badge } from 'react-native-elements';
+import TouchableScale from 'react-native-touchable-scale';
+import { ViewPropTypes } from 'react-native-web';
 
 function MatchesScreen(props) {
     return (
         <View style={styles.container}>
-            <Text>Matches</Text>
+            <ListItem
+            Component={TouchableScale}
+            friction={5}
+            tension={40}
+            activeScale={0.95}
+            button onPress={() => console.log('pressed1')}
+            >
+                <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
+                <ListItem.Content>
+                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
+                    Chris Jackson
+                </ListItem.Title>
+                <ListItem.Subtitle style={{ color: 'black' }}>
+                    Wants to play Football with you!
+                </ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Chevron color="black" />
+                
+            </ListItem>
+            <ListItem
+            Component={TouchableScale}
+            friction={5}
+            tension={40}
+            activeScale={0.95}
+            button onPress={() => console.log('pressed2')}
+            >
+                <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
+                <ListItem.Content>
+                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
+                    Chris Jackson
+                </ListItem.Title>
+                <ListItem.Subtitle style={{ color: 'black' }}>
+                    Wants to play Football with you!
+                </ListItem.Subtitle>
+                </ListItem.Content>
+                    <Badge 
+                    style={styles.badge}
+                    status="error" 
+                    
+                    />
+                
+                
+                    
+                <ListItem.Chevron color="black" />
+                
+                
+            </ListItem>
+            <ListItem
+            Component={TouchableScale}
+            friction={5}
+            tension={40}
+            activeScale={0.95}
+            button onPress={() => console.log("pressed3")}
+            >
+                <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
+                <ListItem.Content>
+                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
+                    Chris Jackson
+                </ListItem.Title>
+                <ListItem.Subtitle style={{ color: 'black' }}>
+                    Wants to play Football with you!
+                </ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Chevron color="black" />
+                
+            </ListItem>
+            <ListItem
+            Component={TouchableScale}
+            friction={5}
+            tension={40}
+            activeScale={0.95}
+            button onPress={() => console.log('pressed4')}
+            onLongPress={() => console.log('long pressed')}
+            >
+                <View>
+                <Avatar rounded source={{ uri: `https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png` }} />
+                <Badge 
+                //value="online"
+                status="success"
+                containerStyle={{ position: 'absolute', top: 0, right: -1 }}
+                />
+                </View>
+                <ListItem.Content>
+                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>
+                    Chris Jackson
+                </ListItem.Title>
+                <ListItem.Subtitle style={{ color: 'black' }}>
+                    Wants to play Football with you!
+                </ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Chevron color="black" />
+                
+                
+
+                
+                
+            </ListItem>
         </View>
     );
 }
@@ -12,7 +112,8 @@ function MatchesScreen(props) {
 const styles = StyleSheet.create({
     container: {
        flex: 1,
-   } 
+   },
+  
 });
 
 export default MatchesScreen;
