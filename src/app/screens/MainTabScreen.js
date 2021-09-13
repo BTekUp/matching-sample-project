453ad72be7e7db4189def5705e4e870fe8c97819
William-Baaklini <76127831+William-Baaklini@users.noreply.github.com>
2021-09-13 01:47:21 -0700

    fixes

--- src/app/screens/MainTabScreen.js
+++ src/app/screens/MainTabScreen.js
@@ -1,19 +1,37 @@
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
-import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
-import HomeScreen from './HomeScreen';
-import MatchesScreen from './MatchesScreen';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import colors from "../shared/colors";
+import HomeScreen from "./HomeScreen";
+import MatchesScreen from "./MatchesScreen";
 
 const Tab = createBottomTabNavigator();
 
-function MainTabScreen({navigation}) {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Matches" component={MatchesScreen} />
-        </Tab.Navigator>
-    );
+function MainTabScreen({ navigation }) {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: () => {
+            return <FontAwesome name="home" size={30} color="grey" />;
+          },
+          showIcon: true,
+        }}
+      />
+      <Tab.Screen
+        name="Matches"
+        component={MatchesScreen}
+        options={{
+          tabBarIcon: () => {
+            return <FontAwesome name="check" size={30} color="grey" />;
+          },
+          showIcon: true,
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default MainTabScreen;
